e6d9363a8c8306aa1896f20fdfbd0253a881839e
Owyn <Owyn@users.noreply.github.com>
2017-08-04 20:37:00 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+
+const	Command = require('command'),		
+		HARROWHOLD = 9950;
+
+
+
+module.exports = function hhp3wall(dispatch) {
+	const command = Command(dispatch)
+	let enabled = true,
+		hooks = [];
+	
+	command.add('hhwall', () => {
+		if(enabled){
+			enabled = false;
+			command.message('HH-P3-FireWall-Replacer module off');
+		}
+		else{
+			enabled = true;
+			command.message('HH-P3-FireWall-Replacer module on');
+		}
+	});
+	
+	dispatch.hook('S_LOAD_TOPO', 1, (event) => {
+		if(event.zone == HARROWHOLD){
+			hook('S_SPAWN_NPC', (event) => {
+				if(enabled && event.templateId == 3018 && event.huntingZoneId == 950)
+				{
+					//console.log("BLOCKED P3 WALL SPAWN");
+					event.templateId = 47946;
+					event.huntingZoneId = 979;
+					return true;
+				}
+				//console.log("S_SPAWN_NPC " + require('util').inspect(event, {depth: null}));
+			});
+		}
+		else
+		{
+			unload()
+		}
+		//console.log("Entering zone: " + event.zone)
+	});
+		
+	/*dispatch.hook('sDespawnNpc', event => {
+		console.log("sDespawnNpc " + require('util').inspect(event, {depth: null}));
+	})*/
+	
+	function unload() {
+		if(hooks.length) {
+			for(let h of hooks) dispatch.unhook(h)
+
+			hooks = []
+		}
+	}
+
+	function hook() {
+		hooks.push(dispatch.hook(...arguments))
+	}
+}
