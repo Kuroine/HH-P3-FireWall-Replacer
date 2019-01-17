0ef4b30ff78c4a5d2a0a16293ed885cc1c0b5578
Owyn <Owyn@users.noreply.github.com>
2019-01-17 17:05:42 +0300

    upd

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ module.exports = function hhp3wall(dispatch) {
 	
 	dispatch.hook('S_LOAD_TOPO', 3, (event) => {
 		if(event.zone == HARROWHOLD){
-			hook('S_SPAWN_NPC', 10, (event) => {
+			hook('S_SPAWN_NPC', dispatch.majorPatchVersion >= 79 ? 11 : 10, (event) => {
 				if(enabled && event.templateId == 3018 && event.huntingZoneId == 950)
 				{
 					//console.log("BLOCKED P3 WALL SPAWN");
